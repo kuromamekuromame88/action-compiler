282e25b74de5430bf24adee16f2aa1efc247d03e
kuromamekuromame88 <tei8sia@outlook.jp>
2025-11-08 19:40:14 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
+#include <Arduino.h>
 
+void setup() {
+    Serial.begin(115200);
+    while (!Serial) { ; }  // シリアル待ち
+    Serial.println("ESP32-S3 USB-OTG test");
+}
+
+void loop() {
+    Serial.println("Hello from ESP32-S3!");
+    delay(1000);
+}
